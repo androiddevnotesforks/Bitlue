0426c8a405f0110dfae84811b9ac4a0282d963c5
Carlos Mesquita <carlos.mesquita.cm2@gmail.com>
2021-03-13 09:46:47 +0100

    Create BitcoinValueFragment
    * Add DI annotation
    * Add its layout resource file

---
+++ app/src/main/java/com/carlosmesquita/technicaltest/n26/bitlue/ui/bitcoin_value/BitcoinValueFragment.kt
@@ -0,0 +1,32 @@
+package com.carlosmesquita.technicaltest.n26.bitlue.ui.bitcoin_value
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.carlosmesquita.technicaltest.n26.bitlue.R
+import com.carlosmesquita.technicaltest.n26.bitlue.databinding.FragmentBitcoinValueBinding
+import com.carlosmesquita.technicaltest.n26.bitlue.ui.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class BitcoinValueFragment : Fragment(R.layout.fragment_bitcoin_value) {
+
+    private val viewModel: MainViewModel by activityViewModels()
+
+    private val binding: FragmentBitcoinValueBinding
+        get() = _binding!!
+    private var _binding: FragmentBitcoinValueBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        _binding = FragmentBitcoinValueBinding.bind(view)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        _binding = null
+    }
+}
