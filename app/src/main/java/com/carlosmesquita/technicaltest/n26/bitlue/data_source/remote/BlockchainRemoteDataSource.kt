9dd9251c7a1b9e65e01b2957487b713629900d3b
Carlos Mesquita <carlos.mesquita.cm2@gmail.com>
2021-03-13 13:19:43 +0100

    Implement BlockchainRemoteDataSource.kt

---
+++ app/src/main/java/com/carlosmesquita/technicaltest/n26/bitlue/data_source/remote/BlockchainRemoteDataSource.kt
@@ -0,0 +1,20 @@
+package com.carlosmesquita.technicaltest.n26.bitlue.data_source.remote
+
+import com.carlosmesquita.technicaltest.n26.bitlue.data_source.DataSource
+import com.carlosmesquita.technicaltest.n26.bitlue.data_source.remote.api.blockchain.BlockchainAPI
+import com.carlosmesquita.technicaltest.n26.bitlue.data_source.remote.api.blockchain.model.BlockchainResponseDTO
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+class BlockchainRemoteDataSource(
+    private val blockchainAPI: BlockchainAPI,
+    private val defaultDispatcher: CoroutineDispatcher = Dispatchers.IO
+) : DataSource<BlockchainResponseDTO> {
+
+    override suspend fun getBitcoinInfo(): Flow<BlockchainResponseDTO> = flow {
+        emit(blockchainAPI.getMarketPriceChart())
+    }.flowOn(defaultDispatcher)
+}
