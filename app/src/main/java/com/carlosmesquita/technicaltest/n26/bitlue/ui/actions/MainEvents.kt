c2871d6d3b0c9876ea0d45c2489a327996d02b1f
Carlos Mesquita <carlos.mesquita.cm2@gmail.com>
2021-03-14 20:38:56 +0100

    Create MainEvents.kt and MainStates.kt
    This will allow an unidirectional data flow between UI and ViewModel
    which fully decouples UI logic and business logic; and organizes better
    all different events and states.

---
+++ app/src/main/java/com/carlosmesquita/technicaltest/n26/bitlue/ui/actions/MainEvents.kt
@@ -0,0 +1,19 @@
+package com.carlosmesquita.technicaltest.n26.bitlue.ui.actions
+
+import com.carlosmesquita.technicaltest.n26.bitlue.data_source.remote.api.blockchain.utils.FilterRollingAverage
+import com.carlosmesquita.technicaltest.n26.bitlue.data_source.remote.api.blockchain.utils.FilterTimeRange
+
+sealed class MainEvents {
+
+    sealed class BitcoinValueEvents : MainEvents() {
+        object OnFabClicked : BitcoinValueEvents()
+    }
+
+    sealed class FilterSettingsEvents : MainEvents() {
+        data class OnFilterTimeRangeClicked(val filterTimeRange: FilterTimeRange) :
+            FilterSettingsEvents()
+
+        data class OnFilterRollingAverageClicked(val filterRollingAverage: FilterRollingAverage) :
+            FilterSettingsEvents()
+    }
+}
